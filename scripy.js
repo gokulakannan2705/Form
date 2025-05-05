8ce2af368d54ca45bcf5e196439a0335e3fa64cc
gokulakannan2705 <gokulakannanp003@gmail.com>
2025-05-05 10:39:20 +0530

    form filler

--- scripy.js
+++ scripy.js
@@ -1,62 +1,88 @@
-cart = []
+let cart = JSON.parse(localStorage.getItem("FormDiv")) || []
+let editId = null
 document.getElementById("FormBox").addEventListener("submit", (e, i) => {
     e.preventDefault()
     let FirstName = (e.target[0].value);
     let LirstName = (e.target[1].value);
     let Email = (e.target[2].value);
     let Id = e.target.i
-    console.log(FirstName, LirstName, Email);
-
-    // e.target[0].value = ""
-    // e.target[1].value = ""
-    // e.target[2].value = ""
-
     if (FirstName === "" || LirstName === "" || Email === "") {
         alert("Please Fill All The Fields")
     }
     else {
-        // id = 0
-        let userDetails = {
-            FirstName,
-            LirstName,
-            Email,
-            Id: cart.length + 1
+        if (editId === null) {
+            let userDetails = {
+                FirstName,
+                LirstName,
+                Email,
+                Id: cart.length + 1
+            }
+            cart.push(userDetails)
+            console.log(cart);
         }
-        cart.push(userDetails)
-        console.log(cart);
+        else {
+           let details= cart.find((v, i) => {
+                return v.Id === editId
+            })            
+            details.FirstName=FirstName
+            details.LirstName=LirstName
+            details.Email=Email
+            editId = null
+
+        }
 
     }
     e.target.reset()
     FormFillers()
-    localStorage.setItem("click", JSON.stringify(cart))
+    localStorage.setItem("FormDiv", JSON.stringify(cart))
 })
 
 
 function FormFillers() {
-    j = 0;
     document.getElementById("TotalStore").innerHTML = cart.map((v, i) => {
-        let { FirstName, LirstName, Email } = v
+        let { FirstName, LirstName, Email, Id } = v
         return `
         <tr>
                         <td>${i + 1}</td>
                         <td>${FirstName}</td>
                         <td>${LirstName}</td>
                         <td>${Email}</td>
-                        <td><button><i class="fa-solid fa-user-pen"></i></button>
-                        <button><i class="fa-solid fa-trash" onclick="delElement(${j})"></i></button></td>
+                        <td><button><i class="fa-solid fa-user-pen" onclick="EditElement(${Id})"></i></button>
+                        <button><i class="fa-solid fa-trash" onclick="delElement(${Id})"></i></button></td>
         </tr>`
 
     }).join("")
-    localStorage.setItem("click", JSON.stringify(cart))
-
 }
 
-function delElement(a) {
+function delElement(Id) {
+    cart = cart.filter((v, i) => {
+        return v.Id !== Id
+    })
 
-    cart.splice(a, 1)
+
+    cart = cart.map((v, i) => {
+        return v ? { ...v, Id: i + 1 } : v;
+    })
+
     FormFillers()
-    console.log(cart);
-    localStorage.setItem("click", JSON.stringify(cart))
+    localStorage.setItem("FormDiv", JSON.stringify(cart))
+
 
 }
 
+FormFillers()
+
+
+function EditElement(Id) {
+
+    let FindElement = cart.find((v, i) => {
+        return v.Id === Id;
+    })
+
+
+    let { FirstName, LirstName, Email } = FindElement
+    document.getElementById("input01").value = FirstName;
+    document.getElementById("input02").value = LirstName;
+    document.getElementById("input03").value = Email;
+    editId = Id
+}
